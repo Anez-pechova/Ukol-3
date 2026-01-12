cb815e2846241c87fb78c870fba5396083a83a00
Anezka <anezka.pechova@gmail.com>
2026-01-12 10:00:57 +0100

    3commit

--- ukol3.js
+++ ukol3.js
@@ -1,10 +1,10 @@
-let jmeno = prompt("Jak se jmenujes?");
+let jmeno = prompt("Jak se jmenuješ?");
 
 if (jmeno === null || jmeno.trim() === "") {
     alert("Nic jste nezadali.");
 } else {
 
-    alert("Ahoj " + jmeno + "!");
+    alert("Ahoj " + jmeno + "!!!");
     
 };
 
